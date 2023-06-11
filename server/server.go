7de1c419481e3c3b1f8bd72ad29bfbd3afe24a31
bozkayasalihx <jamesgosling10@gmail.com>
2023-06-11 20:17:33 +0300

    removed /bin from codebase

--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"fmt"
 	"io"
-	"log"
 	"net"
 
 	"github.com/bozkayasalih01x/cache/cache"
@@ -69,33 +68,7 @@ func (s *Server) handleConnection(conn net.Conn) {
 	fmt.Printf("connection closed with %s", conn.RemoteAddr())
 }
 
-func (s *Server) handleCommand(conn net.Conn, cmd interface{}) {
-	switch v := cmd.(type) {
-	case *proto.MessageGetType:
-		s.handleGetCommand(conn, v)
-	case *proto.MessageSetType:
-		s.handleSetCommand(conn, v)
-	}
+
+func  (s *Server) handleCommand(conn net.Conn, cmd interface{}) {
 }
 
-func (s *Server) handleSetCommand(conn net.Conn, cmd *proto.MessageSetType) {
-	fmt.Printf("key is  -> %s and value is %s \n", cmd.Key, cmd.Value)
-	err := s.c.Set(cmd.Key, cmd.Value)
-	if err != nil {
-		log.Fatalf("couldnt set %s to %s storage", string(cmd.Key), string(cmd.Value))
-
-	}
-}
-
-func (s *Server) handleGetCommand(conn net.Conn, cmd *proto.MessageGetType) {
-	val, err := s.c.Get(cmd.Key)
-	if err != nil {
-		log.Fatalf("couldnt get the data from cache %v  ", err)
-	}
-
-	fmt.Printf("the data from %s connection is  -> %s \n", conn.RemoteAddr(), string(val))
-	_, err = conn.Write(val)
-	if err != nil {
-		panic(err)
-	}
-}
